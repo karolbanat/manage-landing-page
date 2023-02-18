57651544d73c768b7bd0ed976b46f9a8d3a63d44
Karol Banat <karol.banat@gmail.com>
2023-02-18 17:48:40 +0100

    Basic form handling

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,14 @@ import Splide from '@splidejs/splide';
 import '@splidejs/splide/css';
 import './style.css';
 
+const navToggleButton: HTMLButtonElement = document.querySelector('#primary-nav-toggle')!;
+const signUpForm: HTMLFormElement = document.querySelector('#sign-up-form')!;
+const emailInput: HTMLInputElement = signUpForm.querySelector('input[type="email"]')!;
+const submitButton: HTMLButtonElement = signUpForm.querySelector('button[type="submit"]')!;
+
+const EMAIL_REGEX =
+	/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 new Splide('.splide', {
 	start: 1,
 	arrows: false,
@@ -29,9 +37,23 @@ new Splide('.splide', {
 	},
 }).mount();
 
-const navToggleButton: HTMLButtonElement = document.querySelector('#primary-nav-toggle')!;
-
 navToggleButton.addEventListener('click', handleToggleButton);
+submitButton.addEventListener('click', (e: Event) => {
+	e.preventDefault();
+	const email: string = emailInput.value;
+
+	if (isBlank(email)) {
+		displayError(emailInput, 'This field is required');
+		return;
+	}
+
+	if (!isValidEmail(email)) {
+		displayError(emailInput, 'Please insert a valid email');
+		return;
+	}
+
+	clearError(emailInput);
+});
 
 function handleToggleButton(e: Event): void {
 	const button: HTMLButtonElement = e.target as HTMLButtonElement;
@@ -55,4 +77,34 @@ function toggleDropshadow(toggle: boolean) {
 		const dropshadow: HTMLElement | null = document.querySelector('.dropshadow');
 		if (dropshadow) dropshadow.remove();
 	}
+}
+
+function displayError(input: HTMLInputElement, message: string = ''): void {
+	const formGroup: HTMLElement | null = input.closest('.input-group');
+	let error: HTMLElement | null;
+
+	if (formGroup) {
+		formGroup.classList.add('error');
+		error = formGroup.querySelector('.form-error');
+		if (error) error.innerText = message;
+	}
+}
+
+function clearError(input: HTMLInputElement): void {
+	const formGroup: HTMLElement | null = input.closest('.input-group');
+	let error: HTMLElement | null;
+
+	if (formGroup) {
+		formGroup.classList.remove('error');
+		error = formGroup.querySelector('.form-error');
+		if (error) error.innerText = '';
+	}
+}
+
+function isBlank(value: string): boolean {
+	return value === '';
+}
+
+function isValidEmail(email: string): boolean {
+	return EMAIL_REGEX.test(email);
 }
